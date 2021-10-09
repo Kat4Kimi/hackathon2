536a82e5bc57a413a0c60ffa4809f451dbcd6fbd
“Meg <meg5ed@virginia.edu>
2021-10-09 12:25:34 -0600

    added plurals

---
+++ client/src/components/appointment/Appointments.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import AppointmentList from './AppointmentList';
+import AppointmentForm from './AppointmentForm';
+
+const Appointments = () => {
+  const [appointments, setAppointments] = useState([])
+
+  // before it mounts
+  useEffect( () => {
+      // grab Appointments from the database
+      axios.get('/api/Appointments')
+        .then( res => {
+          // and set it to state
+          setAppointments(res.data)
+        })
+        .catch( err => console.log(err) )
+    }, [])
+
+  // add Appointment
+  const addAppointment = (appointment) => {
+    // add in the db
+    // add in the state in the client
+    axios.post('/api/appointments', { appointment })
+      .then(res => {
+        setAppointments([...appointments, res.data])
+      })
+      .catch( err => console.log(err) )
+  }
+
+  // update Appointment
+  const updateAppointment = (id, appointment) => {
+    // update in the db
+    axios.put(`/api/appointments/${id}`, { appointment })
+    .then( res => {
+      // update in the state in the client
+      const updatedAppointments = appointments.map( ap => {
+        if (ap.id == id) {
+          return res.data
+        }
+        return ap
+      })
+      setAppointments(updatedAppointments)
+    })
+    .catch( err => console.log(err) )
+  }
+
+  // delete Appointment
+  const deleteAppointment = (id) => {
+    // delete in the db
+    axios.delete(`/api/appointments/${id}`)
+      .then( res => {
+        // delete in the state in the client
+        setAppointments(appointments.filter( ap => ap.id !== id))
+      })
+      .catch( err => console.log(err) )
+
+  }
+
+  return(
+    <>
+      <AppointmentList 
+        Appointments={Appointments} 
+        deleteAppointment={deleteAppointment}
+        updateAppointment={updateAppointment}
+      />
+      <AppointmentForm addAppointment={addAppointment} />
+    </>
+  )
+}
+
+export default Appointments;
---
+++ client/src/components/pet/Pets.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import petList from './petList';
+import petForm from './petForm';
+
+const Pets = () => {
+  const [pets, setPets] = useState([])
+
+  // before it mounts
+  useEffect( () => {
+      // grab Pets from the database
+      axios.get('/api/pets')
+        .then( res => {
+          // and set it to state
+          setPets(res.data)
+        })
+        .catch( err => console.log(err) )
+    }, [])
+
+  // add pet
+  const addPet = (pet) => {
+    // add in the db
+    // add in the state in the client
+    axios.post('/api/pets', { pet })
+      .then(res => {
+        setPets([...pets, res.data])
+      })
+      .catch( err => console.log(err) )
+  }
+
+  // update pet
+  const updatePet = (id, pet) => {
+    // update in the db
+    axios.put(`/api/pets/${id}`, { pet })
+    .then( res => {
+      // update in the state in the client
+      const updatedPets = pets.map( p => {
+        if (p.id == id) {
+          return res.data
+        }
+        return p
+      })
+      setPets(updatedPets)
+    })
+    .catch( err => console.log(err) )
+  }
+
+  // delete pet
+  const deletePet = (id) => {
+    // delete in the db
+    axios.delete(`/api/pets/${id}`)
+      .then( res => {
+        // delete in the state in the client
+        setPets(pets.filter( p => p.id !== id))
+      })
+      .catch( err => console.log(err) )
+
+  }
+
+  return(
+    <>
+      <petList 
+        Pets={Pets} 
+        deletepet={deletePet}
+        updatepet={updatePet}
+      />
+      <petForm addpet={addPet} />
+    </>
+  )
+}
+
+export default Pets;
